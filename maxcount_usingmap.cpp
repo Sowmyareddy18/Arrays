1b504b61b1d305f2897c55a6b0ab8e12ffb292b2
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-05-24 11:41:26 +0530

    Add files via upload

---
+++ maxcount_usingmap.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	map<int,int>frq;
+	for(int i=0;i<n;i++)
+	{
+		frq[arr[i]]++;
+	}
+	int maxi=0;
+	int id=0;
+	for(auto it:frq)
+	{
+		if(it.second>maxi)
+		{
+			maxi=it.second;
+			id=it.first;
+		}
+	}
+	cout<<"Customer id "<<id<<" arrived max times"<<maxi;
+}
