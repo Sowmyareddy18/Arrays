40730c210f9b2f1f90daa68dd9fab717f5bbed21
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-05-27 09:19:28 +0530

    Add files via upload

---
+++ deleting.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	int n,ele,ind=-1;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	cout<<"enter element to delete";
+	cin>>ele;
+	for(int i=0;i<n;i++)
+	{
+	    if(arr[i]==ele)
+	    {
+	       ind=i;
+	    }
+	}
+	if(ind==-1)
+	{
+	    cout<<"element is not found";
+	    return 0;
+	}
+	for(int i=ind;i<n;i++)
+	{
+	    arr[i]=arr[i+1];
+	}
+	n--;
+	cout<<"array after deleting the element is"<<endl;
+	for(int i=0;i<n;i++)
+	{
+	    cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+/* 
+output:
+5
+10 12 13 14 15
+enter element to delete12
+array after deleting the element is
+10 13 14 15
+*/
