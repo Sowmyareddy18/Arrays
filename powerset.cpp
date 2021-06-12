c43e7fe462a90283f8dcb9b43ce5179bf94661bf
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-12 10:53:27 +0530

    Add files via upload

---
+++ powerset.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int num=0;num<(1<<n);num++)
+	{
+		vector<int>ans;
+		for(int i=0;i<n;i++)
+		{
+			if(num&(1<<i))
+			{
+				ans.push_back(a[i]);
+			}
+		}
+		for(auto it:ans)
+		{
+			cout<<it<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
+/*
+3
+1 2 3
+1 
+2 
+1 2 
+3 
+1 3 
+2 3 
+1 2 3 
+*/
